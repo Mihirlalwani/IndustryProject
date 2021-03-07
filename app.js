bc1d9d3b156c2bee359364a02dd4591041927fc8
Mihirlalwani <mihirlalwani28@gmail.com>
2021-03-07 15:06:31 +0530

    IndustryPro

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+
+const solvent=document.querySelector(".solvent");
+const reset=document.querySelector("#reset");
+const form=document.querySelector("#form");
+
+loadEventListners();
+
+function loadEventListners(){
+form.addEventListener('submit',runTask);
+reset.addEventListener('click',resetPage);}
+
+function runTask(e){
+    e.preventDefault();
+    var outlet= $('#outlet-option').val();
+     if(solvent.value==="" || outlet===[]){
+         alert("Please input Values");
+     }
+     for(let i=0;i<outlet.length;i++){
+    var outletPipe=document.querySelector(".outlet"+outlet[i]);
+    outletPipe.setAttribute('src',"/images/"+solvent.value+"Pipe.png");
+     }
+  
+}
+
+function resetPage(){
+       location.reload();
+}
