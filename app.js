bc09c2e1d6e07634b94f701516ea869c6b04a070
Mihirlalwani <mihirlalwani28@gmail.com>
2021-03-08 18:57:55 +0530

    Added Valves and Removed Multiple option

--- app.js
+++ app.js
@@ -2,29 +2,48 @@ $(document).ready(function(){
     $('select').formSelect();
   });
 
-const solvent=document.querySelector(".solvent");
+const solventA=document.getElementById('outletA');
+const solventB=document.getElementById('outletB');
+const solventC=document.getElementById('outletC');
+const solventD=document.getElementById('outletD');
 const reset=document.querySelector("#reset");
-const form=document.querySelector("#form");
+const run=document.querySelector(".run");
+const pipe=document.querySelectorAll(".opipe")
 
 loadEventListners();
 
 function loadEventListners(){
-form.addEventListener('submit',runTask);
-reset.addEventListener('click',resetPage);}
+  run.addEventListener('click',runTask);
+  reset.addEventListener('click',resetPage);
+}
 
 function runTask(e){
     e.preventDefault();
-    var outlet= $('#outlet-option').val();
-     if(solvent.value==="" || outlet===[]){
-         alert("Please input Values");
-     }
-     for(let i=0;i<outlet.length;i++){
-    var outletPipe=document.querySelector(".outlet"+outlet[i]);
-    outletPipe.setAttribute('src',"images/"+solvent.value+"Pipe.png");
+    
+    if(solventA.value==="" && solventB.value==="" && solventC.value==="" && solventD.value==="" )
+       alert("Enter Outlets");
+    else
+    {
+      if(solventA.value !==""){
+    var outletA=document.querySelector(".outlet"+solventA.value);
+    outletA.setAttribute('src',"images/yellowPipe.png");}
+      if(solventB.value !==""){  
+    var outletB=document.querySelector(".outlet"+solventB.value);
+    outletB.setAttribute('src',"images/redPipe.png");
+       
+  }
+      if(solventC.value !==""){
+    var outletC=document.querySelector(".outlet"+solventC.value);
+    outletC.setAttribute('src',"images/greenPipe.png");}
+      if(solventD.value !==""){
+    var outletD=document.querySelector(".outlet"+solventD.value);
+    outletD.setAttribute('src',"images/bluePipe.png");}
+    }
      }
   
-}
+
 
 function resetPage(){
-       location.reload();
+       console.log(pipe);
 }
+loadEventListners();
